4feade8163c4d12054869ee04d33f2af0029c379
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-10-25 15:30:24 +0300

    Add get token link and routes 4 tockens

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +18,29 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/redirect', function () {
+	$query = http_build_query([
+		'client_id' => 3,
+		'redirect_uri' => 'http://localhost:8000/callback',
+		'response_type' => 'code',
+		'scope' => '',
+	]);
+
+	return redirect('http://localhost:8000/oauth/authorize?'.$query);
+})->name('get.token');
+
+Route::get('/callback',function (Request $request){
+	$guzzle = new GuzzleHttp\Client;
+
+	$response = $guzzle->post('http://localhost:8000/oauth/token', [
+		'form_params' => [
+			'grant_type' => 'client_credentials',
+			'client_id' => 'client-id',
+			'client_secret' => 'client-secret',
+			'scope' => 'your-scope',
+			'code' =>$request->code,
+		],
+	]);
+
+	return json_decode((string) $response->getBody(), true)['access_token'];
+});
